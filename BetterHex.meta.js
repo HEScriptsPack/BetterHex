ae9d193685cc11a62d50825adcaffbb3f3916702
Logfro <moritziphone239@gmail.com>
2017-11-02 10:29:40 +0100

    Update BetterHex.meta.js

--- BetterHex.meta.js
+++ BetterHex.meta.js
@@ -5,5 +5,7 @@
 // @description  Better Hex adds useful functions to the legacy hacker experience
 // @author       Logfro
 // @match        https://legacy.hackerexperience.com/*
+// @updateURL    https://gitcdn.xyz/repo/Logfro/BetterHex/master/BetterHex.meta.js
+// @downloadURL  https://gitcdn.xyz/repo/Logfro/BetterHex/master/BetterHex.user.js
 // @grant        none
 // ==/UserScript==
