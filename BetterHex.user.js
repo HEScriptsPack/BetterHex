aa1976ffa9e17ea7e3072d7bf1fffa1291a98c3c
Logfro <moritziphone239@gmail.com>
2017-11-02 10:24:39 +0100

    Remove your entries script
    adds by the internet log a button which automatically removes your ip out of everything and then commits the log

---
+++ BetterHex.user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         Better HEx by Logfro
+// @namespace    https://logfro.de/
+// @version      0.1
+// @description  Better Hex adds useful functions to the legacy hacker experience
+// @author       Logfro
+// @match        https://legacy.hackerexperience.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function clearLogs(){
+            var elm = document.getElementsByName("log")[0];
+            var x = elm.value;
+            var ownIP = document.getElementsByClassName("header-ip-show")[0].innerHTML;
+            var button = document.getElementsByClassName("btn btn-inverse")[5];
+            x = x.replaceAll(ownIP,"");
+            elm.value = x;
+            button.click();
+    }
+    String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+        return target.replace(new RegExp(search, 'g'), replacement);
+    };
+    function loadLogFunc(){
+        if(document.getElementsByName("log").length > 0){
+            var li = document.createElement("li");
+            var a = document.createElement("a");
+            var span = document.createElement("span");
+            var text = document.createTextNode("Remove your entries");
+            span.className = "hide-phone";
+            a.id = "LogfroLogClickID";
+            li.appendChild(a);
+            a.appendChild(span);
+            span.appendChild(text);
+            li.className = "link";
+            document.getElementsByClassName("nav nav-tabs")[0].appendChild(li);
+            $(document).ready(function(){
+                $("#LogfroLogClickID").on("click", function(){clearLogs();});
+            });
+        }
+    }
+    switch(window.location.href){
+        case "https://legacy.hackerexperience.com/internet?view=logs":
+            if(document.getElementsByClassName("label label-success pull-right").length < 1){
+                loadLogFunc();
+            }
+            break;
+        default:
+            console.log("No");
+            break;
+
+    }
+})();
